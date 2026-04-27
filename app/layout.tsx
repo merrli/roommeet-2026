8bfbc85f00dbc1f5a20d6654a59e992383d05dbd
Vincent Tran <hiimvincent22@gmail.com>
2026-04-27 13:06:25 -0400

    Changed the tab metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "RoomMeet",
+  description: "Meet your future roommate before opening the door",
 };
 
 const geistSans = Geist({
